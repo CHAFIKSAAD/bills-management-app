c9ba3383d4629604663d61bb6ccf522e07eabc36
CHAFIKSAAD <chafiksaad33@gmail.com>
2026-08-03 09:49:39 +0100

    Add global loading indicator

---
+++ frontend/src/components/GlobalLoader.tsx
@@ -0,0 +1,28 @@
+﻿import { useEffect, useState } from "react";
+
+function GlobalLoader() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleLoading = (event: Event) => {
+      const customEvent = event as CustomEvent<boolean>;
+      setLoading(customEvent.detail);
+    };
+
+    window.addEventListener("global-loading", handleLoading);
+
+    return () => {
+      window.removeEventListener("global-loading", handleLoading);
+    };
+  }, []);
+
+  if (!loading) return null;
+
+  return (
+    <div className="global-loader">
+      <div className="global-loader-bar"></div>
+    </div>
+  );
+}
+
+export default GlobalLoader;
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,52 @@
-import axios from "axios";
+﻿import axios from "axios";
+
+let activeRequests = 0;
+
+const showLoader = () => {
+  activeRequests += 1;
+  window.dispatchEvent(new CustomEvent("global-loading", { detail: true }));
+};
+
+const hideLoader = () => {
+  activeRequests -= 1;
+
+  if (activeRequests <= 0) {
+    activeRequests = 0;
+    window.dispatchEvent(new CustomEvent("global-loading", { detail: false }));
+  }
+};
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    showLoader();
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    hideLoader();
+    return Promise.reject(error);
   }
+);
 
-  return config;
-});
+api.interceptors.response.use(
+  (response) => {
+    hideLoader();
+    return response;
+  },
+  (error) => {
+    hideLoader();
+    return Promise.reject(error);
+  }
+);
 
 export default api;
